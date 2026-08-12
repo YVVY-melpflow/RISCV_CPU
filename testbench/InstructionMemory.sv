11810a0623b4a3b9271a9c4c8bd25c89cc98aeb0
YVVY-melpflow <yvvy.melpflow@gmail.com>
2026-08-13 05:50:44 +0900

    Parametarize memory size

--- testbench/InstructionMemory.sv
+++ testbench/InstructionMemory.sv
@@ -3,14 +3,16 @@ module InstructionMemory (
   output logic [31:0] o_data
 );
 
-  // 1KB (4byte x 256)
-  logic [31:0] mem [0:255];
+  parameter  DATA_NUM   = 8; // This value should be a power of 2.
+  localparam ADDR_WIDTH = $clog2(DATA_NUM);
+
+  logic [31:0] mem [DATA_NUM];
 
   initial begin
     $readmemh("../memory/imem.txt", mem);
   end
 
-  assign o_data = (i_addr[31:10] == 22'h0) ? mem[i_addr[9:2]]
-                                           : 32'h0000_0000;
+  assign o_data = (i_addr[31:(2 + ADDR_WIDTH)] == '0) ? mem[i_addr[2 +: ADDR_WIDTH]]
+                                                      : 32'h0000_0000;
 
 endmodule
